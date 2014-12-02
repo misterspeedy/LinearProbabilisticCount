fcd1be89e1a1ce5caad054f2c5cc0053e2549279
Kit <kit.eason@gmail.com>
2014-12-02 13:05:37 +0000

    Add parallel counting WIP
    Tests don't pass

--- LinearProbabilisticCount/LinearProbabilistic.fs
+++ LinearProbabilisticCount/LinearProbabilistic.fs
@@ -1,5 +1,7 @@
 ﻿namespace BigDataCounting
 
+open FSharp.Collections.ParallelSeq
+
 module LinearProbabilistic =
 
    /// Gets the hash code of an object but cast to uint32.
@@ -11,24 +13,61 @@ module LinearProbabilistic =
    /// when the code exceeds the stated maximum. 
    /// Has a higher probability of collisions than the pure hash.
    let WrappedHash max o =
-      (o |> Uint32Hash) % max
+      if max = 0u then
+         raise (new System.ArgumentException("Max must be > 0"))
+      else
+         (o |> Uint32Hash) % max
 
    /// Produces a linear probabilistic count of the unique items
    /// in the given sequence, using the specified size for the
    /// bitmap.  Accuracy increases with larger map sizes.
    ///
    /// http://highscalability.com/blog/2012/4/5/big-data-counting-how-to-count-a-billion-distinct-objects-us.html
-   let Count (mapSizeBytes : int) (s : seq<'T>) =
-      let map = Array.zeroCreate mapSizeBytes
-
-      let mapSizeBits = mapSizeBytes * 8
+   let private CreateMap (mapSizeBits : int) (s : seq<'T>) =
+      let map = Array.zeroCreate (mapSizeBits*8)
 
       s
       |> Seq.map (WrappedHash (uint32 mapSizeBits))
       |> Seq.iter (Bitmap.SetBit map)
 
+      map
+
+   let private EstimateFromMap map = 
       let w = map |> Bitmap.OneCount |> double
-      let m = mapSizeBits |> double
+      // TODO avoid recalculating length
+      let m = map |> Seq.length |> double
 
       -m * System.Math.Log((m-w)/m)
+
+   // TODO remove mapSizeBytes/Bits confusion and always work in bits
+   let Count (mapSizeBytes : int) (s : seq<'T>) =
+      let mapSizeBits = mapSizeBytes * 8
+
+      let map = CreateMap mapSizeBits s
+
+      EstimateFromMap map
+
+   let PCount (mapSizeBytes : int) (parallelism : int) (s : seq<'T>) =
+      let mapSizeBits = mapSizeBytes * 8
+      // Divide the sequence up into n sections
+      let numbered =
+         s |> Seq.mapi (fun i x -> i, x)
+      let sections =
+         [0..parallelism-1]
+         // TODO PSeq
+         |> Seq.map (fun x -> numbered |> Seq.filter (fun (i, _) -> i % parallelism = 0) |> Seq.map snd)
+
+      // Separately and in parallel compute the bitmaps
+      let maps =
+         sections 
+         // TODO PSeq
+         |> PSeq.map (fun s -> s |> CreateMap mapSizeBits |> Seq.ofArray)
+      
+      // Or all the bitmaps together
+      let unifiedMap = maps |> Bitmap.AllOr
+
+      // Count the number of bits in the Or'd bitmap
+      // Apply the standard estimation formula
+      EstimateFromMap unifiedMap
+
 
---
+++ LinearProbabilisticCountTests/LinearProbabilistic.Tests.fs
@@ -0,0 +1,93 @@
+﻿[<NUnit.Framework.TestFixture>]
+module LinearProbabalisticTests
+
+open NUnit.Framework
+open FsUnit
+open BigDataCounting
+
+// Uint32Hash 
+
+[<TestCase(0, 0u)>]
+[<TestCase(1, 1u)>]
+[<TestCase(-1, 4294967295u)>]
+let ``Uint32Hash takes an int32 hash and produces an equivalent uint32 hash``(int32Hash, expected) =
+   let actual = int32Hash |> LinearProbabilistic.Uint32Hash
+   actual |> should equal expected
+
+// WrappedHash
+
+[<TestCase(0, 10u, 0u)>]
+[<TestCase(1, 10u, 1u)>]
+[<TestCase(1000000, 1000001u, 1000000u)>]
+let ``WrappedHash takes an int32 hash and produces an equivalent uint32 hash when the stated max is not reached``(int32Hash, max, expected) =
+   let actual = int32Hash |> LinearProbabilistic.WrappedHash max
+   actual |> should equal expected
+
+[<TestCase(10, 10u, 0u)>]
+[<TestCase(11, 10u, 1u)>]
+[<TestCase(1000000, 1000000u, 0u)>]
+[<TestCase(1000002, 1000000u, 2u)>]
+[<TestCase(2000002, 1000000u, 2u)>]
+let ``WrappedHash takes an int32 hash and produces an equivalent uint32 hash when the stated max is equalled``(int32Hash, max, expected) =
+   let actual = int32Hash |> LinearProbabilistic.WrappedHash max
+   actual |> should equal expected
+
+[<Test>]
+let ``WrappedHash raises an exception when provided with a max of zero``() =
+   let cant = (fun () -> 99 |> LinearProbabilistic.WrappedHash 0u |> ignore)
+   cant |> should throw typeof<System.ArgumentException>
+
+// Count
+
+let private GuidsSequence count =
+   seq {
+      for _ in 1..count do
+         yield System.Guid.NewGuid()
+   }
+
+let private GuidsSequenceRepeated cardinality count =
+   let r = new System.Random(1)
+   if count % cardinality <> 0 then
+      raise (new System.ArgumentException("Count must be divisible by cardinality"))
+
+   let innerSeq = GuidsSequence cardinality |> Seq.cache
+
+   seq {
+      for _ in 1..count/cardinality -> innerSeq
+   }
+   |> Seq.concat
+   |> Seq.sortBy (fun _ -> r.Next())
+
+[<TestCase(1000, 10000, 1E-3, 4)>]
+[<TestCase(1000, 1000, 1E-2, 4)>]
+[<TestCase(1000, 100, 1E-1, 4)>]
+[<TestCase(10000, 10000, 1E-1, 100)>]
+[<TestCase(10000, 1000, 5.0, 100)>]
+let ``Given a sequence with a given cardinality and a specific map size Count produces an estimate within a specific margin``(totalCount, mapSize, margin, expected) =
+   let cardinality = expected
+   let guids = GuidsSequenceRepeated cardinality (totalCount * cardinality)
+   let actual = guids |> LinearProbabilistic.Count mapSize 
+   actual |> should (equalWithin margin) expected
+
+// PCount
+
+[<TestCase(1000, 10000, 1E-3, 8, 4)>]
+[<TestCase(1000, 1000, 1E-2, 8, 4)>]
+[<TestCase(1000, 100, 1E-1, 8, 4)>]
+[<TestCase(10000, 10000, 1E-1, 8, 100)>]
+[<TestCase(10000, 1000, 5.0, 8, 100)>]
+let ``Given a sequence with a given cardinality map size and degree of parallelism PCount produces an estimate within a specific margin``(totalCount, mapSize, margin, parallelism, expected) =
+   let cardinality = expected
+   let guids = GuidsSequenceRepeated cardinality (totalCount * cardinality)
+   let actual = guids |> LinearProbabilistic.PCount mapSize parallelism
+   actual |> should (equalWithin margin) expected
+
+
+
+// TODO have a specific exception message for map size too small instead of infinity
+// TODO return an accuracy estimate as part of the return results?
+
+// FSharp.Collections.ParallelSeq  |> PSEq
+
+
+
